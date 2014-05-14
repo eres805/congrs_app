6f906f9f32b6ee91fd89a748cb27ef065ad85310
eres805 <eres805@gmail.com>
2014-05-14 18:21:06 -0400

    Senators and Reps Controllers working

--- app/controllers/senators_controller.rb
+++ app/controllers/senators_controller.rb
@@ -1,7 +1,28 @@
 class SenatorsController < ApplicationController
+
+  before_filter :determine_scope 
+  before_filter :current_state, :only => :index
+
   def index
+  	@senators = @scope.all
+  	@state = @state_name
   end
 
   def show
+  	@senator = Senator.find(params[:id])
   end
+
+  private
+
+  	def current_state
+  		@current_state = State.find(params[:state_id])
+  		@state_name = @current_state.name
+  	end
+
+    def determine_scope
+      @scope = if params[:state_id]
+        State.find(params[:state_id]).senators
+      end
+    end
+  
 end
