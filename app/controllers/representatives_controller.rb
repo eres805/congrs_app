72c46e1181edd4ac41b5037f24366d43fa73e24b
eres805 <eres805@gmail.com>
2014-05-14 20:15:56 -0400

    accordian added

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,7 +1,8 @@
 class RepresentativesController < ApplicationController
  
-  before_filter :determine_scope 
+  before_filter :determine_scope, :only => :index 
   before_filter :current_state, :only => :index
+  # before_filter :rep_state, :only => :show
 
   def index
     @representatives = @scope.all
@@ -10,9 +11,21 @@ class RepresentativesController < ApplicationController
 
   def show
     @representative = Representative.find(params[:id])
+    # @state = @state_name
+    
+      respond_to do |format|
+      format.html { }
+      format.json { render json: {msg: params[:data]} }
+      end
   end
 
   private
+
+    # def rep_state
+    #   @representative_state_id = @representative.state_id
+    #   @rep_state = State.where(state_id: @representative_state_id)
+    #   @state_name = @rep_state.name
+    # end
 
     def current_state
       @current_state = State.find(params[:state_id])
@@ -21,8 +34,9 @@ class RepresentativesController < ApplicationController
 
     def determine_scope
       @scope = if params[:state_id]
-        State.find(params[:state_id]).representatives
-      end
+      State.find(params[:state_id]).representatives
     end
     
+  end
+
 end
